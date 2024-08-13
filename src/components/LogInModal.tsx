44283995f0c28f7496c961e28d21ca2a1869c9fd
Moon Geonwoo <moongunwoo7019@gmail.com>
2024-08-14 02:29:27 +0900

    fix: 그냥 이쁘게

--- src/components/LogInModal.tsx
+++ src/components/LogInModal.tsx
@@ -65,47 +65,41 @@ export default function LogInModal({ isOpen, onClose }: ILogInModalProps) {
         <ModalHeader>Log In</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Box pb={"5"} as="form" onSubmit={handleSubmit(onSubmit)}>
-            <VStack>
-              <InputGroup>
-                <InputLeftElement>
-                  <FaRegUser />
-                </InputLeftElement>
-                <Input
-                  {...register("username", {
-                    required: "아이디를 입력해주세요",
-                  })}
-                  placeholder="아이디"
-                  isInvalid={Boolean(errors.username?.message)}
-                />
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement>
-                  <TbLock />
-                </InputLeftElement>
-                <Input
-                  {...register("password", {
-                    required: "비밀번호를 입력해주세요",
-                  })}
-                  type="password"
-                  placeholder="비밀번호"
-                  isInvalid={Boolean(errors.password?.message)}
-                />
-              </InputGroup>
-              {mutation.isError && (
-                <Text color={"red.500"} fontSize={"sm"}>
-                  아이디나 비밀번호가 틀렸습니다.
-                </Text>
-              )}
-              <Button
-                type="submit"
-                width={"100%"}
-                isLoading={mutation.isPending}
-              >
-                로그인
-              </Button>
-            </VStack>
-          </Box>
+          <VStack pb={"5"} as="form" onSubmit={handleSubmit(onSubmit)}>
+            <InputGroup>
+              <InputLeftElement>
+                <FaRegUser />
+              </InputLeftElement>
+              <Input
+                {...register("username", {
+                  required: true,
+                })}
+                placeholder="아이디"
+                isInvalid={Boolean(errors.username?.message)}
+              />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement>
+                <TbLock />
+              </InputLeftElement>
+              <Input
+                {...register("password", {
+                  required: true,
+                })}
+                type="password"
+                placeholder="비밀번호"
+                isInvalid={Boolean(errors.password?.message)}
+              />
+            </InputGroup>
+            {mutation.isError && (
+              <Text color={"red.500"} fontSize={"sm"}>
+                아이디나 비밀번호가 틀렸습니다.
+              </Text>
+            )}
+            <Button type="submit" width={"100%"} isLoading={mutation.isPending}>
+              로그인
+            </Button>
+          </VStack>
           <Box position={"relative"} py={4}>
             <Divider />
             <AbsoluteCenter px={4}>또는</AbsoluteCenter>
